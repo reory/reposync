46e499a6221d3405216e03b769d4c15b662df774
Roy Peters <reory35@hotmail.com>
2026-03-25 22:39:05 +0000

    Updated rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,13 @@
 import reflex as rx
+import os
+
+# This looks for the URL we set in the Render Environment tab
+# If it can't find it, it defaults to localhost (so it still works for you locally!)
+app_url = os.getenv("REFLEX_BACKEND_URL", "http://localhost:8000")
 
 config = rx.Config(
     app_name="reposync",
+    api_url=app_url,  # THIS IS THE MISSING LINK
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
